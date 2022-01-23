9f372837e689352a52b992dc5cbf3a52f4f31a01
nick <nick.turner82@gmail.com>
2022-01-22 21:16:52 -0800

    update deployment vars

--- standard/deployment.tf
+++ standard/deployment.tf
@@ -1,11 +1,11 @@
 module "standard" {
   source                = "./module"
-  aws_access_key        = ""
-  aws_secret_key        = ""
-  aws_key_pair          = ""
-  aws_region            = "us-east-1"
+  #aws_access_key        = ""
+  #aws_secret_key        = ""
+  aws_key_pair          = "${var.aws_key_pair}"
+  aws_region            = "us-west-1"
   aws_domain_name       = "kasm.iworksometimes.com"
-  project_name          = "iworksometimes"
+  project_name          = "kasm"
   num_agents            = "1"
   num_webapps           = "1"
 
@@ -18,11 +18,11 @@ module "standard" {
 
 
   ssh_access_cidr        = "0.0.0.0/0"
-  database_password     = "changeme"
-  redis_password        = "changeme"
-  user_password         = "changeme"
-  admin_password        = "changeme"
-  manager_token         = "changeme"
+  database_password     = "${var.database_password}"
+  redis_password        = "${var.redis_password}"
+  user_password         = "${var.user_password}"
+  admin_password        = "${var.admin_password}"
+  manager_token         = "${var.manager_token}"
   zone_name             = "default"
   kasm_build            = "https://kasm-static-content.s3.amazonaws.com/kasm_release_1.9.0.077388.tar.gz"
 }
